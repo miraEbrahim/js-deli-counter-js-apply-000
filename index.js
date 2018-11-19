fb3b1988ec22db9b06839312869e554d4d62c46f
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 11:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ var customerAndNumber = [];
 }
 function nowServing (katzDeliLine) { 
     if (katzDeliLine.length === 0) {
-   return 'The line is currently empty.'; 
+   return 'There is nobody waiting to be served!'; 
  }
     var customer = katzDeliLine.shift();
     
