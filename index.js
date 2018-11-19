9f0a4621583c7f456c4cc0140312703b823310dd
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 11:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var katzDeli = [];
 var takeANumber = function(katzDeli , newCustomer) {
-  if (katzDeli.length > 0 ){
+  if (katzDeli.length > 0 );
     katzDeli.push(newCustomer);
     return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
-  }
+
 } 
 
 takeANumber(katzDeli,'Ada');
