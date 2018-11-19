c1c381ed7dc4ff66b21eeb063de92585d45f800d
maryamxebrahim <maryamxebrahim@gmail.com>
2018-11-19 11:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 var takeANumber = function(katzDeli,newCustomer) {
   if (katzDeli.length > 0 ){
     katzDeli.push(newCustomer);
-    return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + 'in line.')
+    return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
 } 
 
@@ -46,5 +46,5 @@ function nowServing (katzDeliLine) {
    return "Currently serving " + customer + "." ;
 }
 
-const deliLine = ["Steven", "Blake", "Avi"];
+const deliLine = ["Ada", "Grace", "Kent"];
 console.log(deliLine);
